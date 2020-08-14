80086b129d613d153fb90d5b53223abadea89e63
niewiadomski <agnieszka.niewiadomski@saaswedo.com>
2020-08-14 18:21:23 +0200

    🛸 gitignore

---
+++ PictIonis/PictIonis/ViewControllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PictIonis
+//
+//  Created by Agnieszka Niewiadomski on 11/08/2020.
+//  Copyright © 2020 niewiajuzain_ad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
